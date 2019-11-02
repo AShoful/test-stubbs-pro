16d7a05b106520292b7228a460bf44d728080043
Angrey Shoful <shoful.a@gmail.com>
2019-11-02 18:14:31 +0200

    Updates

---
+++ precache-manifest.51a71e30ad81614275e7f30313cbdd71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ced33ea5b85213422948f29e1f429744",
+    "url": "/test-stubbs-pro/index.html"
+  },
+  {
+    "revision": "597be53707d955fde492",
+    "url": "/test-stubbs-pro/static/css/main.7b30b631.chunk.css"
+  },
+  {
+    "revision": "dd104305433f928bfa7b",
+    "url": "/test-stubbs-pro/static/js/2.e2227674.chunk.js"
+  },
+  {
+    "revision": "597be53707d955fde492",
+    "url": "/test-stubbs-pro/static/js/main.2ff2f29a.chunk.js"
+  },
+  {
+    "revision": "0bb9918a0db5731dfa43",
+    "url": "/test-stubbs-pro/static/js/runtime~main.334b0046.js"
+  }
+]);
